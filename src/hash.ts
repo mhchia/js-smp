6342d6b92daae02baadcf098267539827ef0fe49
mhchia <kevin.mh.chia@gmail.com>
2020-04-16 15:35:08 +0800

    Add `hashToInt`

---
+++ src/hash.ts
@@ -0,0 +1,14 @@
+import BN from 'bn.js';
+import { sha256 } from 'js-sha256';
+
+const endian = "be";  // big endian
+const intSize = 1536 / 8;  // bytes
+
+export function hashToInt(version: BN, ...args: BN[]): BN {
+  const versionBytes: number[] = version.toArray(endian, intSize);
+  let res: number[] = versionBytes;
+  for (const arg of args) {
+    res = res.concat(arg.toArray(endian, intSize));
+  }
+  return new BN(sha256(res), "hex");
+}
---
+++ test/hash.test.ts
@@ -0,0 +1,17 @@
+import BN from 'bn.js';
+import { hashToInt } from '../src/hash';
+
+describe('version', () => {
+  test('hardcoded test', () => {
+    expect(hashToInt(new BN(1))).toEqual(new BN("57153b76f61c2badabeb23203d690a2fe5509e76d1196658af70b2daf62c1aec", "hex"));
+  });
+  test('versions serve as the prefixes', () => {
+    const data1 = new BN(1);
+    const data2 = new BN(2);
+    const data1HashVersion1 = hashToInt(new BN(1), data1);
+    const data1HashVersion2 = hashToInt(new BN(2), data1);
+    const data2HashVersion1 = hashToInt(new BN(1), data2);
+    expect(data1HashVersion1.eq(data1HashVersion2)).toBeFalsy();
+    expect(data1HashVersion1.eq(data2HashVersion1)).toBeFalsy();
+  });
+});
