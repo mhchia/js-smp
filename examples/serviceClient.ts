05f8da51c00a119609311b61125e1d480df4a2e2
mhchia <kevin.mh.chia@gmail.com>
2020-04-28 23:30:06 +0800

    Add TODO for examples

--- examples/serviceClient.ts
+++ examples/serviceClient.ts
@@ -5,6 +5,7 @@ import { SMPService } from '../src/service';
 const IP = '127.0.0.1';
 const PORT = 3000;
 
+// TODO: Receive `ip`, `port`, and `secret` from args.
 async function main() {
   const client = new SMPService(new BN(1));
   const result = await client.connect(IP, PORT);
--- examples/serviceServer.ts
+++ examples/serviceServer.ts
@@ -5,8 +5,10 @@ import { SMPService } from '../src/service';
 const IP = '127.0.0.1';
 const PORT = 3000;
 
+// TODO: Receive `ip`, `port`, and `secret` from args.
 async function main() {
   const server = new SMPService(new BN(1));
+  // TODO: Handle Ctrl-C, which calls `close`.
   server.listen(IP, PORT);
 }
 
