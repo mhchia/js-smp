7364a7663126509e502cdea5481a52ed4772865c
mhchia <kevin.mh.chia@gmail.com>
2020-05-30 17:54:47 +0800

    Prettier

--- src/config.ts
+++ src/config.ts
@@ -6,11 +6,11 @@ import { MultiplicativeGroup } from './multiplicativeGroup';
 const modulusInt = new BN(MODULUS.replace('\n', '').replace(' ', ''), 'hex');
 
 type Config = {
-  modulus: BN;  // The modulus for the multiplicative group
-  modulusSize: number;  // Number of bytes a modulus occupies
-  q: BN;  // The modulus when generating proofs
-  g: MultiplicativeGroup;  // The generator of the group
-  endian: 'be' | 'le';  // The order of bytes for a binary representation
+  modulus: BN; // The modulus for the multiplicative group
+  modulusSize: number; // Number of bytes a modulus occupies
+  q: BN; // The modulus when generating proofs
+  g: MultiplicativeGroup; // The generator of the group
+  endian: 'be' | 'le'; // The order of bytes for a binary representation
 };
 
 const defaultConfig: Config = {
